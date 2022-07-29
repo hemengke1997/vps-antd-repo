1539b7bb647194b1864eb5ff4a158c4daae2c4c8
Minko <49073282+hemengke1997@users.noreply.github.com>
2022-07-29 18:26:09 +0800

    update

--- start/index.js
+++
@@ -1,2 +0,0 @@
-global.__vite_server_start_time = performance.now()
-global.__vite_dom_mounted = false
--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,6 @@ import ssr from 'vite-plugin-ssr/plugin'
 export default {
   plugins: [
     react(),
-    ssr(),
-    {
-      name: 'log-dom-mount-time',
-      apply: 'serve',
-      transformIndexHtml(_, { server }) {
-        if (!global.__vite_dom_mounted) {
-          const info = server.config.logger.info
-
-          const viteStartTime = global.__vite_server_start_time || false
-          const startupDurationString = viteStartTime ? `DOM ready in ${(Number(Math.ceil(performance.now() - viteStartTime)) / 1000).toFixed(2)} s` : ''
-
-          info(`\n🍃 ${startupDurationString}\n`)
-          global.__vite_dom_mounted = true
-        }
-      },
-    },
+    ssr()
   ],
 }
